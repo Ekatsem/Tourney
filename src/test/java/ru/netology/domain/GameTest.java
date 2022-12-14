42155770ca75eaf219a71e9d8b8f30468a80eb12
Ekaterina <kat.sem@mail.ru>
2022-12-14 18:33:58 +0300

    add src

---
+++ src/test/java/ru/netology/domain/GameTest.java
@@ -0,0 +1,80 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+    @Test
+    public void testWhenFirstStronger(){
+        Game game = new Game();
+        Player player1 = new Player(1, "Ivan", 40);
+        Player player2 = new Player(2, "Anna", 30);
+
+        game.register(player1);
+        game.register(player2);
+
+        int expected = 1;
+        int actual = game.round("Ivan", "Anna");
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testWhenSecondStronger(){
+        Game game = new Game();
+        Player player1 = new Player(1, "Ivan", 20);
+        Player player2 = new Player(2, "Anna", 30);
+
+        game.register(player1);
+        game.register(player2);
+
+        int expected = 2;
+        int actual = game.round("Ivan", "Anna");
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testWhenSameStrength(){
+        Game game = new Game();
+        Player player1 = new Player(1, "Ivan", 30);
+        Player player2 = new Player(2, "Anna", 30);
+
+        game.register(player1);
+        game.register(player2);
+
+        int expected = 0;
+        int actual = game.round("Ivan", "Anna");
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testWhenFirstUnregistered(){
+        Game game = new Game();
+        Player player1 = new Player(1, "Ivan", 40);
+        Player player2 = new Player(2, "Anna", 30);
+
+        game.register(player2);
+
+        Assertions.assertThrows(RuntimeException.class, () -> game.round("Ivan", "Anna"));
+    }
+    @Test
+    public void testWhenSecondUnregistered(){
+        Game game = new Game();
+        Player player1 = new Player(1, "Ivan", 40);
+        Player player2 = new Player(2, "Anna", 30);
+
+        game.register(player1);
+
+        Assertions.assertThrows(RuntimeException.class, () -> game.round("Ivan", "Anna"));
+    }
+    @Test
+    public void testWhenNoOneRegistered(){
+        Game game = new Game();
+        Player player1 = new Player(1, "Ivan", 40);
+        Player player2 = new Player(2, "Anna", 30);
+
+        Assertions.assertThrows(RuntimeException.class, () -> game.round("Ivan", "Anna"));
+    }
+
+}
